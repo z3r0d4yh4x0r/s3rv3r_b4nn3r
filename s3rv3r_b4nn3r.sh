9cbe8f4368595d4d59f4b1263c0a561bd7a08018
z3r0d4yh4x0r <130175798+z3r0d4yh4x0r@users.noreply.github.com>
2023-05-06 16:41:44 +0530

    Add files via upload

---
+++ s3rv3r_b4nn3r.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+######################################
+# 		Author : Z3R0D4YH4X0R		 #
+######################################
+
+# usage: ./s3rv3r_b4nn3r.sh <File containing LIST of URL>
+
+input=$1
+
+RED='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+NC='\033[0m' # No Color
+
+
+while read -r line
+do
+  server=`curl -sI $line | grep -wi server`
+  if test -z "$server" ;then
+   	printf "$line : ${Green}NOT DETECTED${NC}\n"
+  else
+  	server_version=`echo "$server" | awk '/[[:digit:]]/'`
+  	if test -n  "$server_version" ;then
+   		printf "$line : ${RED}$server_version${NC}\n"
+   	else
+   		[[ "$server" =~ ^[^0-9]+$ ]] && printf "$line : ${Yellow}$server${NC}\n"
+   	fi
+  fi
+done < "$input"
+
